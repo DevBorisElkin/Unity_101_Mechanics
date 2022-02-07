85026e4fddc05e1e50eb9709d577f5e6f176dcf0
DevBorisElkin <ea.foreverrr@gmail.com>
2022-02-07 19:35:40 +0300

    Scrolling code cleanup

--- Assets/Core/General/Scripts/UI/SnapScrolling.cs
+++ Assets/Core/General/Scripts/UI/SnapScrolling.cs
@@ -43,6 +43,9 @@ public class SnapScrolling : MonoBehaviour
     
     
         public ScrollRect scrollRect;
+
+        public bool allowScrollingThrough;
+        public float minInertiaCap = 400;
         
         void Start()
         {
@@ -60,28 +63,13 @@ public class SnapScrolling : MonoBehaviour
                 if (i == 0) continue;
                 instPans[i].transform.localPosition = new Vector2(instPans[i-1].transform.localPosition.x + panelPrefab.GetComponent<RectTransform>().sizeDelta.x + panelOffset,
                      instPans[i].transform.localPosition.y);
-                pansPos[i] = new Vector2(-instPans[i].GetComponent<RectTransform>().sizeDelta.x - panelOffset, 0f);
+                pansPos[i] = new Vector2((-instPans[i].GetComponent<RectTransform>().sizeDelta.x - panelOffset) * i, 0f);
             }
         }
         
         void ApplyData(int i)
         {
-           //RawImage[] result = instPans[i].GetComponentsInChildren<RawImage>();
-           //btnImages[i] = result[1];
-           //setRawTexture(result[0], uni_load.levelsList.ElementAt(i).texture);
-           //Text[] txtResult = instPans[i].GetComponentsInChildren<Text>();
-           //txtResult[0].text = uni_load.levelsList.ElementAt(i).level;        // setting str name
-           //
-           //txtResult[1].text = uni_load.levelsList.ElementAt(i).introducingTo;  //setting str description
-           //
-           //
-           //txtResult[2].text += uni_load.levelsList.ElementAt(i).difficulty;  // setting difficulty
-        
-           //txtResult[3].text = getCoinsAmountCollected(uni_load.levelsList.ElementAt(i).level_index) + "/20"; // setting coins left
-        
-        
-           //manageAccess(i);
-        
+           
         }
         
             public void SwapLevel(bool right)
@@ -114,13 +102,26 @@ public class SnapScrolling : MonoBehaviour
                 contentRect.localPosition = new Vector2(0 + 25000, contentRect.localPosition.y);
             }
         
+            bool forbidSettingPanelId;
+            float timeForWhichForbidToSetPanelId = 1f;
+            
             void FixedUpdate()
             {
+                CapInertiaOnBorders();
+                ManageScaling();
+                SelectNearestPanelId();
+                if(ManageScrollingMinSpeed())
+                    PerformAutoScroll();
+            }
+
+            void CapInertiaOnBorders()
+            {
                 if(!isScrolling && (contentRect.anchoredPosition.x >=pansPos[0].x || contentRect.anchoredPosition.x <= pansPos[pansPos.Length - 1].x))
-                {
                     scrollRect.inertia = false;
-                }
-        
+            }
+            
+            void ManageScaling()
+            {
                 //for(int i = 0; i<panelCount; i++)
                 //{
                 //    float distance = Math.Abs(contentRect.anchoredPosition.x - pansPos[i].x);
@@ -129,12 +130,13 @@ public class SnapScrolling : MonoBehaviour
                 //    pansScale[i].x = Mathf.SmoothStep(instPans[i].transform.localScale.x, scale+scaleY_adding, scaleSpeed * Time.fixedDeltaTime);
                 //    pansScale[i].y = Mathf.SmoothStep(instPans[i].transform.localScale.x, scale+scaleY_adding, scaleSpeed * Time.fixedDeltaTime);
                 //    instPans[i].transform.localScale = pansScale[i];
-        //
-                //    manageButton(scale, i); //removed for a while
+                //
+                //    
                 //}
-
-                
-                
+            }
+            
+            void SelectNearestPanelId()
+            {
                 float nearestPos = float.MaxValue;
                 int closestPanId = 0;
                 for (int i = 0; i < panelCount; i++)
@@ -145,69 +147,40 @@ public class SnapScrolling : MonoBehaviour
                         nearestPos = distance;
                         closestPanId = i;
                     }
-                    Debug.Log($"nearestPos[{nearestPos}], contentRect.anchoredPosition.x[{contentRect.anchoredPosition.x}], pansPos[{i}].x[{pansPos[i].x}]");
                 }
                 selectedPanelID = closestPanId;
+            }
 
+            bool ManageScrollingMinSpeed()
+            {
                 float scrollVelocity = Mathf.Abs(scrollRect.velocity.x);
-                if (scrollVelocity < 400 && !isScrolling) scrollRect.inertia = false;
-                if (isScrolling || scrollVelocity > 400) return;
+                if (allowScrollingThrough)
+                {
+                    if (scrollVelocity < minInertiaCap && !isScrolling)
+                    {
+                        scrollRect.inertia = false;
+                        scrollRect.velocity = Vector2.zero;
+                    }
+                    else if (isScrolling || scrollVelocity > minInertiaCap)
+                        return false;
+                }
+                else
+                {
+                    if (isScrolling)
+                        return false;
+                }
+
+                return true;
+            }
+
+            void PerformAutoScroll()
+            {
                 contentVector.x = Mathf.SmoothStep(contentRect.anchoredPosition.x, pansPos[selectedPanelID].x, snapSpeed*Time.fixedDeltaTime);
                 contentRect.anchoredPosition = contentVector;
-            }
-        
-            void manageButton(float scale, int i)
-            {
-                float paintSpeed = 0.30f;
-                float eraseSpeed = 10f;
-        
-                float paintSpeedPanel = 0.26f;
-                float eraseSpeedPanel = 6f;
-        // if (scale<0.9f)
-        // {
-        //     instPans[i].GetComponentsInChildren<RawImage>()[1].color = new Color(255, 255, 255, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<RawImage>()[1].color.a, 0, eraseSpeed * Time.fixedDeltaTime));
-        //     /*
-        //     instPans[i].GetComponentsInChildren<RawImage>()[2].color = new Color(255, 255, 255, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<RawImage>()[1].color.a, 0, eraseSpeed * Time.fixedDeltaTime));
-        //     
-        //     Color colorCurrent = instPans[i].GetComponentsInChildren<UnityEngine.UI.Image>()[1].color;
-        //     instPans[i].GetComponentsInChildren<UnityEngine.UI.Image>()[1].color = new Color(colorCurrent.r, colorCurrent.g, colorCurrent.b, Mathf.SmoothStep(colorCurrent.a, 0, eraseSpeedPanel * Time.fixedDeltaTime));
-        //
-        //
-        //     instPans[i].GetComponentsInChildren<Text>()[1].color = new Color(50, 50, 50, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<Text>()[1].color.a, 0, eraseSpeedPanel * Time.fixedDeltaTime));
-        //     instPans[i].GetComponentsInChildren<Text>()[2].color = new Color(50, 50, 50, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<Text>()[2].color.a, 0, eraseSpeedPanel * Time.fixedDeltaTime));
-        //
-        //     instPans[i].GetComponentsInChildren<Text>()[3].color = new Color(50, 50, 50, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<Text>()[2].color.a, 0, eraseSpeedPanel * Time.fixedDeltaTime));
-        //     */
-        // }// scale != 1.18f
-        // else if(scale!= 0.6800002f)
-        // {
-        //     instPans[i].GetComponentsInChildren<RawImage>()[1].color = new Color(255, 255, 255, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<RawImage>()[1].color.a, 255, paintSpeed * Time.fixedDeltaTime));
-        //     /*
-        //     instPans[i].GetComponentsInChildren<RawImage>()[2].color = new Color(255, 255, 255, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<RawImage>()[1].color.a, 255, paintSpeed * Time.fixedDeltaTime));
-        //     
-        //     Color colorCurrent = instPans[i].GetComponentsInChildren<UnityEngine.UI.Image>()[1].color;
-        //     instPans[i].GetComponentsInChildren<UnityEngine.UI.Image>()[1].color = new Color(colorCurrent.r, colorCurrent.g, colorCurrent.b, Mathf.SmoothStep(0, 4000, paintSpeed * Time.fixedDeltaTime));
-        //
-        //     instPans[i].GetComponentsInChildren<Text>()[1].color = new Color(50, 50, 50, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<Text>()[1].color.a, 255, paintSpeed * Time.fixedDeltaTime));
-        //     instPans[i].GetComponentsInChildren<Text>()[2].color = new Color(50, 50, 50, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<Text>()[2].color.a, 255, paintSpeed * Time.fixedDeltaTime));
-        //
-        //     instPans[i].GetComponentsInChildren<Text>()[3].color = new Color(50, 50, 50, Mathf.SmoothStep(instPans[i].GetComponentsInChildren<Text>()[2].color.a, 255, paintSpeed * Time.fixedDeltaTime));
-        //     */
-        // }
             }
             public void Scrolling(bool scroll)
             {
                 isScrolling = scroll;
-                if (scroll) scrollRect.inertia = true;
-            }
-        
-            void setTexture(GameObject obj, Texture texture)
-            {
-                obj.GetComponent<RawImage>().texture = texture;
-            }
-        
-            void setRawTexture(RawImage raw, Texture texture)
-            {
-                raw.texture = texture;
+                if (scroll && allowScrollingThrough) scrollRect.inertia = true;
             }
 }
